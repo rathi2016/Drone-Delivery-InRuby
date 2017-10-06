7b4f9850d61692b35d8c57241e87eb9013de9279
Rathi <github email address>
2017-10-06 12:50:04 -0400

    added fetchData method

--- drone_delivery.rb
+++ drone_delivery.rb
@@ -5,6 +5,37 @@ require_relative './helpers_required.rb'
 class Drone_Delivery < HelperFunctions
     include UrlPath
 
+    def initialize
+    # initializing empty drones & packages
+      @drone = [ ]
+      @packages = [ ]
+      @drone = nil
+      @packge = nil
+    #Queues for drones and packages
+    #1 adding package in queue based on deadline
+      @packagesQueue = [ ]
+    #2 adding drones in queue based on how quickly they can pick up their next package
+      @dronesQueue = [ ]
+    end
+    # function for converting given deadline into read-able format(time)
+    def intToTime(deadline)
+       Time.at(deadline)
+    end
+
+
+    def fetchData
+     @drones = get_drone_info
+     @packages = get_package_info
+     for package in @packages
+      # read-able deadline
+       package["deadline"]= intToTime(package["deadline"])
+       distance = distance_between(DEPO,package["destination"])
+      # divided by 50/km per hour drone's speed; this will return us duration, in hours
+       package["deliveryTime"] = distance / DRONE_SPEED
+     end
+      puts @packages
+    end
 end
 
 drone = Drone_Delivery.new
+drone.fetchData
--- url_required.rb
+++ url_required.rb
@@ -6,13 +6,13 @@ module UrlPath
 
   def get_drone_info
     uri = URI.parse("https://codetest.kube.getswift.co/drones")
-    response = Net::HTTP.get_response(uri)
-    response.body
+    response = Net::HTTP.get(uri)
+    JSON.parse(response)
   end
   def get_package_info
     uri = URI.parse("https://codetest.kube.getswift.co/packages")
-    response = Net::HTTP.get_response(uri)
-    response.body
+    response = Net::HTTP.get(uri)
+    JSON.parse(response)
   end
 
 end
